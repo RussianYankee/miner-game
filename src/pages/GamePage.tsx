a4b0aae584bd357b5e2fb6e7a464c3b908ac3796
Anton Evlampeev <a.evlampeev@gmail.com>
2025-02-25 00:14:34 -0600

    unused import removed

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useGameSetup } from '../hooks/useGameSetup';
 import { GameConfig } from '../components/GameControls';
 import GameBoard from '../components/GameBoard';
